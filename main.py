bf6f966e42d61c11f763c0dfea8044bc565b117a
KOOKIIE <25145447+KOOKIIEStudios@users.noreply.github.com>
2022-05-18 16:16:48 +0800

    FEAT: Add logger

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 """Simple bot implementation for the Composer or Pasta game
 """
+import logger
+
+
+kookiie_logger = logger.get_logger("main")
+kookiie_logger.info("COMPOSER OR PASTA TELEGRAM BOT")
+kookiie_logger.info("===============================")
+kookiie_logger.info("Logger successfully loaded in main.")
+
 
 with open("token.txt", "r") as token_file:
 	TOKEN = token_file.read()
+if not TOKEN:
+	kookiie_logger.error("Token is empty! Please add the Telegram Bot token to token.txt!")
+else:
+	kookiie_logger.info("Token successfully loaded in main.")
 
-	
